65298a3280201dd7c00d1b7989b0a826c1939472
muradmy00 <murad.my00@gmail.com>
2024-08-05 21:14:19 +0600

    game

---
+++ script.js
@@ -0,0 +1,88 @@
+const choices = ["rock","paper","scissors"];
+
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const finalResult = document.getElementById("finalResult");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = "";
+
+    if(playerChoice === computerChoice){
+
+        result = "IT'S A TIE!";
+    }
+    else{
+
+         switch(playerChoice){
+
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!":"YOU LOSE!";
+                break;
+            
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!":"YOU LOSE!";
+                break;
+
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!":"YOU LOSE!";
+                break;
+            
+            
+         } 
+         
+    }
+
+    playerDisplay.textContent = `PlAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+
+    resultDisplay.textContent = result;
+
+
+    resultDisplay.classList.remove("greenText","redText");
+
+switch(result){
+
+    case "YOU WIN!":
+        resultDisplay.classList.add("greenText");
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        break;
+    
+    case "YOU LOSE!":
+        resultDisplay.classList.add("redText");
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        break;
+
+    }
+
+    if(playerScore ==  10){
+
+        finalResult.textContent= `FINAL RESULT: PLAYER IS WIN!`;
+        
+    }
+    if(computerScore ==  10){
+
+        finalResult.textContent= `FINAL RESULT: COMPUTER IS WIN!`;
+        
+    }
+    if(playerScore == 10 || computerScore==10){
+
+        setTimeout(function() {
+    location.reload();
+}, 3000);
+
+
+    }
+    
+
+}
